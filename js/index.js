4506da17bd55ef8420111eaeab14625d377c8090
strannik67 <73234679+strannik67@users.noreply.github.com>
2023-10-19 13:47:10 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const testimonailsSwiper = new Swiper(".testimonials__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  grabCursor: true,
+});
+
+const teamSwiper = new Swiper(".team__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  grabCursor: true,
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# IT Website
+
+## Описание
+
+Учебный проект
+
+## Используемые технологии
+
+- HTML
+- CSS
+- БЭМ
+- PerfectPixel
+- Swiper.js
+
+---
+
+[Demo](https://it-website.nothingisreal.ru/)
